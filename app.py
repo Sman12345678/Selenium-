48beac941550408e9ca28ebb922e2b71dc0abc8a
Suleiman <Suleimanayomide716@gmail.com>
2025-05-13 10:46:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException,TimeoutExceptation
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 import subprocess
 import os
 import traceback
