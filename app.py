cdf93f87fc7a2d087d669f5dc12f5bf5f2a05311
Suleiman <Suleimanayomide716@gmail.com>
2025-06-17 14:36:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 
 ADMIN_CODE = "ICU14CU"
 
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
 
 app = Flask(__name__)
 
